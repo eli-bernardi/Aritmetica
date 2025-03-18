5fbae48229c840a8e2d429bc955022c515cd3bb2
Eliel <elielbernardi0012@gmail.com>
2025-03-18 11:48:58 -0300

    Programando a soma

---
+++ prog.js
@@ -0,0 +1,8 @@
+let resposta = document.getElementById('resposta')
+function calcular(){
+    let num1 = Number(document.getElementById('num1').value)
+    let num2 = Number(document.getElementById('num2').value)
+
+    let soma = num1 + num2
+    resposta.innerHTML = 'Resultado da Soma = ' + soma
+}
